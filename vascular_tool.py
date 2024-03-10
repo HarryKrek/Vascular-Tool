7e31bee18fc93fd6a35355478209982a763d9005
Harry Krek <uqhkrek@eait.uq.edu.au>
2024-03-10 13:55:27 +1000

    Added segmentation changes to the vascular tool python run

--- vascular_tool.py
+++ vascular_tool.py
@@ -5,7 +5,6 @@ from skimage.color import rgb2gray, label2rgb
 from skimage.morphology import skeletonize, remove_small_objects, remove_small_holes, isotropic_dilation
 from skimage.filters import gaussian, threshold_local
 import numpy as np
-from dsepruning import skel_pruning_DSE
 from scipy.ndimage import distance_transform_edt
 from alive_progress import alive_bar
 import pandas as pd
@@ -15,7 +14,8 @@ import argparse
 import sknw
 import networkx as nx
 import tifffile
-from skimage.exposure import equalize_hist
+from skimage.exposure import adjust_gamma
+import plantcv.plantcv as pcv
 
 
 def find_images_in_path(pathdir):
@@ -29,31 +29,29 @@ def find_images_in_path(pathdir):
 def get_running_approval():
     pass
 
-def import_and_blur_image(imgPath, sigma = 0.5):
+def import_and_blur_image(imgPath, sigma = 2.5):
     img = io.imread(imgPath)
     imgGrey = img[:,:,1] #Take the green channel
-    adapted_hist = equalize_hist(imgGrey)
+    adapted_hist = adjust_gamma(imgGrey)
     blurred = gaussian(adapted_hist, sigma=(sigma, sigma), truncate=3.5, channel_axis=-1)
     return img, blurred
 
 def segment_image(blurred):
-    thresh = threshold_local(blurred , block_size = 301) 
+    thresh = threshold_local(blurred , block_size = 301)
     #Apply segmentation threshold
     segmentation = blurred > thresh
     return segmentation
 
-def remove_holes_and_small_items(segmentation, min_object_size = 100, min_hole_size = 100):
-    ensmallend = remove_small_objects(segmentation, min_size = 160, connectivity=8)
-    unholed = remove_small_holes(ensmallend, area_threshold = 100)
+def remove_holes_and_small_items(segmentation, min_object_size = 1000, min_hole_size = 300):
+    ensmallend = remove_small_objects(segmentation, min_size = min_object_size, connectivity=8)
+    unholed = remove_small_holes(ensmallend, area_threshold = min_hole_size)
     return unholed
 
-def create_skeleton(segmentation, area_min = 100):
+def create_skeleton(segmentation, line_min = 30):
     #Skeletonisation
-    skel = skeletonize(segmentation)
-    #DSE Pruning
-    skel_dist = distance_transform_edt(segmentation,return_indices=False, return_distances=True)
-    pruned_skel = skel_pruning_DSE(skel,skel_dist,area_min).astype(np.uint16)
-    return pruned_skel
+    skel = pcv.morphology.skeletonize(segmentation)
+    pruned_skeleton, img, objects = pcv.morphology.prune(skel_img=skel, size=line_min)
+    return pruned_skeleton
 
 def draw_and_save_images(image, segmentation,name):
     #Make an image
@@ -83,7 +81,7 @@ def vessel_statistics_from_graph(graph):
     for (s,e) in graph.edges():
         ps = graph[s][e]['weight']
         totalLen += ps
-    
+
     return totalLen, totalLen/len(graph.edges())
 
 def process_image_results(segmentation, graph):
@@ -104,36 +102,35 @@ def process_image_results(segmentation, graph):
     except Exception as e:
         results["Errors"] = str(e)
     return results
-    
+
 def save_results_to_csv(savename,data):
     df=pd.DataFrame(data)
     df.to_csv(savename)
 
 
 def main(path: str, savename: str):
-    path = "\\\\shares01.rdm.uq.edu.au\\HKUG2023-A10939\\20230304_075556_96 wel plate_2D co culture_ HAEC P2_ASC52 P8_20230303_4X_TIME LAPSE\\WellC6\\F2\\"
-    savename = "test.csv"
+    path = "F://F2//"
+    savename = ".\\test.csv"
     resultsPath = '.\\Results\\'
     images = find_images_in_path(path)
     results = [] #list of dictionaries
     try:
         with alive_bar(len(images)) as bar:
             for i, image in enumerate(images):
-                if i % 10 == 0:
-                    rgbimg, blurred = import_and_blur_image(image)
-                    segmentation = segment_image(blurred)
-                    #cleaned_segmentation = remove_holes_and_small_items(segmentation)
-                    skel = create_skeleton(segmentation)
-                    graph = sknw.build_sknw(skel)
-                    img_results = process_image_results(segmentation, graph)
-                    print(img_results)
-                    results.append(img_results)
-                    #draw_and_save_images((rgbimg * 255).astype(np.uint8),
-                    #    segmentation, resultsPath + str(i)+'.tiff')
+                rgbimg, blurred = import_and_blur_image(image)
+                segmentation = segment_image(blurred)
+                cleaned_segmentation = remove_holes_and_small_items(segmentation)
+                skel = create_skeleton(cleaned_segmentation)
+                graph = sknw.build_sknw(skel)
+                img_results = process_image_results(segmentation, graph)
+                print(img_results)
+                results.append(img_results)
+                #draw_and_save_images((rgbimg * 255).astype(np.uint8),
+                #    segmentation, resultsPath + str(i)+'.tiff')
                 bar()
     except Exception as e:
         print(e)
-            
+
     save_results_to_csv(savename, results)
 
 
