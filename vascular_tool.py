ccbc9dd1ec4c04c9c119155661732342b215aa95
Harry Krek <uqhkrek@eait.uq.edu.au>
2024-04-08 15:38:55 +1000

    Completeyl working segmetation, rescale intensity, equalize adapthist, local theshold

--- vascular_tool.py
+++ vascular_tool.py
@@ -2,9 +2,9 @@
 import matplotlib.pyplot as plt
 from skimage import data, io
 from skimage.color import rgb2gray, label2rgb
-from skimage.morphology import skeletonize, remove_small_objects, remove_small_holes, isotropic_dilation
+from skimage.morphology import skeletonize, remove_small_objects, remove_small_holes, isotropic_dilation, isotropic_erosion, disk
 from skimage.filters import gaussian, threshold_local, median
-from skimage.exposure import equalize_adapthist
+from skimage.exposure import equalize_adapthist, rescale_intensity
 import numpy as np
 from scipy.ndimage import distance_transform_edt
 from alive_progress import alive_bar
@@ -35,54 +35,57 @@ def get_global_threshold(images):
         if i % 10 == 0:
             img = skimage.io.imread(imageLoc)
             imgGrey = img[:,:,1] #Take the green channel
-            imgAdapted = (skimage.exposure.equalize_adapthist(imgGrey,nbins=256)*255).astype(np.uint8)
+            imgAdapted1 = rescale_intensity(imgGrey)
+            imgAdapted = (skimage.exposure.equalize_adapthist(imgAdapted1,nbins=256)*255).astype(np.uint8)
 
             #get the values in bin form
             imgArray = imgAdapted.ravel()
             imageHistoN, _ = np.histogram(imgArray, bins=binVals)
             histoTotal = histoTotal + imageHistoN
     #Use k-means clustering to determine the global threshold
-    kMeansHisto, _ = kmeans1d.cluster(histoTotal, 2)
-    #Get position of changes in kmeans -> cluster
-    clusterChange = np.where(np.roll(kMeansHisto,1)!=kMeansHisto)
-    return np.max(clusterChange)
+    # yen_thresh = threshold_yen(histoTotal)
+    return None
 
 def get_running_approval():
     pass
 
-def import_and_blur_image(imgPath, sigma = 2.5):
+def import_and_blur_image(imgPath, sigma = 0.5):
     img = io.imread(imgPath)
     imgGrey = img[:,:,1] #Take the green channel
-    adapted_hist = equalize_adapthist(imgGrey)
+    rescaled_grey = rescale_intensity(imgGrey)
+    adapted_hist = equalize_adapthist(rescaled_grey)
     blurred = gaussian(adapted_hist, sigma=(sigma, sigma), truncate=3.5, channel_axis=-1)
     return img, blurred
 
 def segment_image(blurred, thresh):
     #Convert to uint8
     uint8Image = (blurred * 255).astype(np.uint8)
-    # thresh = threshold_local(blurred , block_size = 301)
-    #Apply segmentation threshold
+    thresh = threshold_local(uint8Image, block_size=301)
+    #Apply segmentation threshold 
     segmentation = uint8Image > thresh
     return segmentation
 
-def remove_holes_and_small_items(segmentation, min_object_size = 100, min_hole_size = 100):
-    #Median filter
-    medianFiltered = median(segmentation)
-    # ensmallend = remove_small_objects(medianFiltered, min_size = min_object_size, connectivity=8)
-    unholed = remove_small_holes(medianFiltered, area_threshold = min_hole_size)
+def remove_holes_and_small_items(segmentation, min_object_size = 8, min_hole_size = 5):
+    #erode a bit
+    eroded = isotropic_erosion(segmentation, 1)
+    ensmallend = remove_small_objects(eroded, min_size = min_object_size, connectivity=8)
+    dilated = isotropic_dilation(ensmallend, 1)
+    unholed = remove_small_holes(dilated, area_threshold = min_hole_size)
     return unholed
 
-def create_skeleton(segmentation, line_min = 30):
+def create_skeleton(segmentation, line_min = 10):
     #Skeletonisation
     skel = pcv.morphology.skeletonize(segmentation)
     pruned_skeleton, img, objects = pcv.morphology.prune(skel_img=skel, size=line_min)
     return pruned_skeleton
 
-def draw_and_save_images(image, segmentation, name):
+def draw_and_save_images(image, segmentation, branchPoints, endPoints, name):
     #Make an image
     masked = label2rgb(segmentation,image=image, colors = ['red'], alpha=0.6, saturation = 1)
-    # plt.imshow(masked)
     adjusted = (masked * 255).astype(np.uint8)
+    fig, ax = plt.subplots()
+    ax.imshow(adjusted)
+    # ax.plot()
     #Save image, not working too well at the moment+
     skimage.io.imsave(name,adjusted)
     
@@ -129,7 +132,7 @@ def process_image_results(segmentation, graph):
         results["Errors"] = ""
     except Exception as e:
         results["Errors"] = str(e)
-    return results
+    return results, branchPoints, endPoints
 
 def save_results_to_csv(savename,data):
     df=pd.DataFrame(data)
@@ -138,25 +141,26 @@ def save_results_to_csv(savename,data):
 
 def main(path: str, savename: str):
     path = "F://20230304_075556_96 wel plate_2D co culture_ HAEC P2_ASC52 P8_20230303_4X_TIME LAPSE//Wellc9//F2"
-    savename = ".\\08_04_24_test_global_threshold_WELL_C9.csv"
+    savename = ".\\08_04_24_test_new_cleaning_thresh_well_C9.csv"
     resultsPath = './Results/'
     images = find_images_in_path(path)
     results = [] #list of dictionaries
-    globalThresh = get_global_threshold(images)
+    # globalThresh = get_global_threshold(images)
+    globalThresh = None
     try:
         with alive_bar(len(images)) as bar:
             for i, image in enumerate(images):
-                if i % 10 ==0:
+                if i % 5 ==0:
                     rgbimg, blurred = import_and_blur_image(image)
-                    segmentation = segment_image(blurred,globalThresh)
+                    segmentation = segment_image(blurred, globalThresh)
                     cleaned_segmentation = remove_holes_and_small_items(segmentation)
                     skel = create_skeleton(cleaned_segmentation)
                     graph = sknw.build_sknw(skel)
-                    img_results = process_image_results(segmentation, graph)
+                    img_results, branchPoints, endPoints = process_image_results(cleaned_segmentation, graph)
                     print(img_results)
                     results.append(img_results)
                     draw_and_save_images(rgbimg,
-                       segmentation, os.path.abspath(resultsPath + str(i)+'.tiff'))
+                       cleaned_segmentation, branchPoints, endPoints, os.path.abspath(resultsPath + str(i)+'.tiff'))
                 bar()
     except Exception as e:
         print(e)
