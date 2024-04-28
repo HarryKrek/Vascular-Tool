f291da89f48006d46020b82c168e81b6b73aa463
Harry Krek <uqhkrek@eait.uq.edu.au>
2024-04-28 12:09:51 +1000

    black formatted

--- vascular_tool.py
+++ vascular_tool.py
@@ -1,8 +1,15 @@
-#TODO clean up whatever tf is going on with my imports, this is getting excessive
+# TODO clean up whatever tf is going on with my imports, this is getting excessive
 import matplotlib.pyplot as plt
 from skimage import data, io
 from skimage.color import rgb2gray, label2rgb
-from skimage.morphology import skeletonize, remove_small_objects, remove_small_holes, isotropic_dilation, isotropic_erosion, disk
+from skimage.morphology import (
+    skeletonize,
+    remove_small_objects,
+    remove_small_holes,
+    isotropic_dilation,
+    isotropic_erosion,
+    disk,
+)
 from skimage.filters import gaussian, threshold_local, median
 from skimage.exposure import equalize_adapthist, rescale_intensity
 import numpy as np
@@ -18,16 +25,17 @@ import skimage
 import kmeans1d
 import plantcv.plantcv as pcv
 import os
-from multiprocessing import Pool,cpu_count,set_start_method
+from multiprocessing import Pool, cpu_count, set_start_method
 import pyyaml
 
 
 def find_images_in_path(pathdir):
     path = Path(pathdir)
-    images = list(path.glob('*.tif'))
+    images = list(path.glob("*.tif"))
     print(f"{len(images)} images found in directory")
     images.sort()
     return images
+
 
 # def get_global_threshold(images):
 #     #preallocate zeros for all the bins
@@ -48,57 +56,72 @@ def find_images_in_path(pathdir):
 #     # yen_thresh = threshold_yen(histoTotal)
 #     return None
 
+
 def get_running_approval():
     pass
 
-def import_and_blur_image(imgPath, sigma = 1):
+
+def import_and_blur_image(imgPath, sigma=1):
     img = io.imread(imgPath)
-    imgGrey = img[:,:,1] #Take the green channel
+    imgGrey = img[:, :, 1]  # Take the green channel
     rescaled_grey = rescale_intensity(imgGrey)
     adapted_hist = equalize_adapthist(rescaled_grey)
-    blurred = gaussian(adapted_hist, sigma=(sigma, sigma), truncate=3.5, channel_axis=-1)
+    blurred = gaussian(
+        adapted_hist, sigma=(sigma, sigma), truncate=3.5, channel_axis=-1
+    )
     return img, blurred
 
+
 def segment_image(blurred):
-    #Convert to uint8
+    # Convert to uint8
     uint8Image = (blurred * 255).astype(np.uint8)
     thresh = threshold_local(uint8Image, block_size=101)
-    #Apply segmentation threshold 
+    # Apply segmentation threshold
     segmentation = uint8Image > thresh
     return segmentation
 
-def remove_holes_and_small_items(segmentation, min_object_size = 300, min_hole_size = 170):
-    #erode a bit
+
+def remove_holes_and_small_items(segmentation, min_object_size=300, min_hole_size=170):
+    # erode a bit
     eroded = isotropic_erosion(segmentation, 1)
-    ensmallend = remove_small_objects(eroded, min_size = min_object_size, connectivity=8)
+    ensmallend = remove_small_objects(eroded, min_size=min_object_size, connectivity=8)
     dilated = isotropic_dilation(ensmallend, 1)
-    unholed = remove_small_holes(dilated, area_threshold = min_hole_size)
+    unholed = remove_small_holes(dilated, area_threshold=min_hole_size)
     return unholed
 
-def create_skeleton(segmentation, line_min = 10):
-    #Skeletonisation
+
+def create_skeleton(segmentation, line_min=10):
+    # Skeletonisation
     skel = pcv.morphology.skeletonize(segmentation)
     pruned_skeleton, img, objects = pcv.morphology.prune(skel_img=skel, size=line_min)
     return pruned_skeleton
 
-def draw_and_save_images(image, segmentation, bp, ep, skel, name, save=True, show = False):
-    #Make an image
-    masked = label2rgb(segmentation,image=image, colors = ['red'], alpha=0.6, saturation = 1)
+
+def draw_and_save_images(
+    image, segmentation, bp, ep, skel, name, save=True, show=False
+):
+    # Make an image
+    masked = label2rgb(
+        segmentation, image=image, colors=["red"], alpha=0.6, saturation=1
+    )
     adjusted = (masked * 255).astype(np.uint8)
     fig, ax = plt.subplots()
     ax.imshow(adjusted)
-    plt.scatter([point[1] for point in bp], [point[0] for point in bp], color = 'blue', s=5)
-    plt.scatter([point[1] for point in ep], [point[0] for point in ep], color = 'green', s=5)
+    plt.scatter(
+        [point[1] for point in bp], [point[0] for point in bp], color="blue", s=5
+    )
+    plt.scatter(
+        [point[1] for point in ep], [point[0] for point in ep], color="green", s=5
+    )
     plt.imshow(skel > 0, alpha=0.8)
-    #Save image, not working too well at the moment+
+    # Save image, not working too well at the moment+
     # skimage.io.imsave(name,adjusted)
-    ax.axis('off')
-    plt.savefig(name, bbox_inches='tight', pad_inches=0, transparent=True)
+    ax.axis("off")
+    plt.savefig(name, bbox_inches="tight", pad_inches=0, transparent=True)
     if show:
         plt.show()
     else:
         plt.close()
-    
 
 
 def obtain_branch_and_end_points(graph):
@@ -111,19 +134,20 @@ def obtain_branch_and_end_points(graph):
             ep.append(node)
         elif graph.degree[node] > 2:
             bp.append(node)
-    branchPoints = np.array([nodes[i]['o'] for i in bp])
-    endPoints = np.array([nodes[i]['o'] for i in ep])
+    branchPoints = np.array([nodes[i]["o"] for i in bp])
+    endPoints = np.array([nodes[i]["o"] for i in ep])
 
     return (branchPoints, endPoints)
 
 
 def vessel_statistics_from_graph(graph):
     totalLen = 0
-    for (s,e) in graph.edges():
-        ps = graph[s][e][0]['weight'] 
+    for s, e in graph.edges():
+        ps = graph[s][e][0]["weight"]
         totalLen += ps
 
-    return totalLen, totalLen/len(graph.edges())
+    return totalLen, totalLen / len(graph.edges())
+
 
 def process_image_results(i, segmentation, graph):
     try:
@@ -137,16 +161,20 @@ def process_image_results(i, segmentation, graph):
         # Area of Vessels
         results["vesselArea"] = np.count_nonzero(segmentation)
         # Percentage Area
-        results["percentArea"] = results["vesselArea"]/imgSize * 100
-        results["totalVesselLength"], results["avgVesselLength"] = vessel_statistics_from_graph(graph)
+        results["percentArea"] = results["vesselArea"] / imgSize * 100
+        results["totalVesselLength"], results["avgVesselLength"] = (
+            vessel_statistics_from_graph(graph)
+        )
         results["Errors"] = ""
     except Exception as e:
         results["Errors"] = str(e)
     return results, branchPoints, endPoints
 
-def save_results_to_csv(savename,data):
-    df=pd.DataFrame(data)
+
+def save_results_to_csv(savename, data):
+    df = pd.DataFrame(data)
     df.to_csv(savename)
+
 
 def worker_process(args):
     try:
@@ -157,10 +185,18 @@ def worker_process(args):
         cleaned_segmentation = remove_holes_and_small_items(segmentation)
         skel = create_skeleton(cleaned_segmentation)
         graph = sknw.build_sknw(skel, multi=True, iso=False, ring=True, full=True)
-        img_results, branchPoints, endPoints = process_image_results(i, cleaned_segmentation, graph)
+        img_results, branchPoints, endPoints = process_image_results(
+            i, cleaned_segmentation, graph
+        )
         print(img_results)
-        draw_and_save_images(rgbimg,
-            cleaned_segmentation, branchPoints, endPoints, skel, os.path.abspath(resultsPath + str(i)+'.png'))
+        draw_and_save_images(
+            rgbimg,
+            cleaned_segmentation,
+            branchPoints,
+            endPoints,
+            skel,
+            os.path.abspath(resultsPath + str(i) + ".png"),
+        )
         return img_results
     except Exception as e:
         print(f"EXCEPTION: {e}")
@@ -169,10 +205,10 @@ def worker_process(args):
 def main(path: str, savename: str):
     path = "F://20230304_075556_96 wel plate_2D co culture_ HAEC P2_ASC52 P8_20230303_4X_TIME LAPSE//Wellc8//F2"
     savename = ".\\24_04_well_c8_ASH_speckle_and_holes.csv"
-    resultsPath = './Results/'
+    resultsPath = "./Results/"
     images = find_images_in_path(path)
-    results = [] #list of dictionaries
-    args = [(i, image, resultsPath) for i,image in enumerate(images)]
+    results = []  # list of dictionaries
+    args = [(i, image, resultsPath) for i, image in enumerate(images)]
     set_start_method("spawn")
     with Pool(cpu_count()) as p:
         results = p.map(worker_process, args)
@@ -181,11 +217,12 @@ def main(path: str, savename: str):
     save_results_to_csv(savename, results)
 
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Vascular Tool for Microscopy Analysis')
-    parser.add_argument('-c', '--config')
-    parser.add_argument('-p', '--path')
-    parser.add_argument('-s', '--savename')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Vascular Tool for Microscopy Analysis"
+    )
+    parser.add_argument("-c", "--config")
+    parser.add_argument("-p", "--path")
+    parser.add_argument("-s", "--savename")
     args = parser.parse_args()
     main(args.path, args.savename)
