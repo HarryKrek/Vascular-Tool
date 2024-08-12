f0965a6fc476fb831f759a6c2cbe7c94e3c44677
Harry Krek <harry.krek@gmail.com>
2024-08-12 21:14:44 +1000

    New GUI Milestone

--- gui.py
+++ gui.py
@@ -23,25 +23,34 @@ class MyScrollableCheckboxFrame(ctk.CTkScrollableFrame):
 
 class App(ctk.CTk):
     def __init__(self):
-        #Initiate super
+        # Initiate super
         super().__init__()
             
-        #Create Grid
+        # Create Grid
         self.title("BRAT -  Vascular Image Analysis")
         self.geometry(f"{1100}x{580}")
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=2)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=0)  # Added a new row for the bottom frame
+        
+        # Create sidebar Frame
+        self.sidebar_frame = ctk.CTkFrame(self, width=140, corner_radius=3)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=3, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(3, weight=1)
 
-        #Textbox Name
-        self.nameTag = ctk.CTkTextbox(self,state = "disabled")
-        self.nameTag.insert("0.0", "BRAT")
-        self.nameTag.grid(row = 0, column=0, sticky = "nsw")
+        # Place Items in Sidebar Frame
+        # Firstly BRAT Frame
+        self.logo = ctk.CTkLabel(self, width=120, height=120, corner_radius=1, text="BRAT", font=ctk.CTkFont(size=20, weight="bold"))
+        self.logo.grid(row=0, column=0, sticky="new", padx=10, pady=10)
 
-        #Scrollable Checkbox Frame
-        values = ["value 1", "value 2", "value 3", "value 4", "value 5", "value 6"]
-        self.scrollable_checkboxFrame = MyScrollableCheckboxFrame(self, title = "User Settings", values =values)
-        self.scrollable_checkboxFrame.grid(row = 0, column = 1, pady = 10, sticky = "nsew", columnspan = 2)
+        # Place loading bar frame at the bottom
+        self.bottom_frame = ctk.CTkFrame(self, corner_radius=3)
+        self.bottom_frame.grid(row=2, column=0, columnspan=2, sticky="ew")  # Spanning across both columns
+        self.bottom_frame.grid_columnconfigure(0, weight=1)  # Adjusted configuration
+
+
 
 if __name__ == '__main__':
     app = App()
